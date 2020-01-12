34c37b3765aa008cb8c4f1d502116c61c4539616
Ivan Gagarinov <dzencot@gmail.com>
2020-01-12 12:14:45 +0500

    add search path

--- chapter-10/solveRPN.hs
+++ chapter-10/solveRPN.hs
@@ -3,9 +3,9 @@ solveRPN = head . foldl foldingFunction [] . words
   where
     foldingFunction (x:y:ys) "*" = (x * y):ys
     foldingFunction (x:y:ys) "+" = (x + y):ys
-    foldingFunction (x:y:ys) "-" = (x - y):ys
-    foldingFunction (x:y:ys) "/" = (x / y):ys
-    foldingFunction (x:y:ys) "^" = (x ** y):ys
+    foldingFunction (x:y:ys) "-" = (y - x):ys
+    foldingFunction (x:y:ys) "/" = (y / x):ys
+    foldingFunction (x:y:ys) "^" = (y ** x):ys
     foldingFunction (x:xs) "ln" = log x:xs
     foldingFunction xs "sum" = [sum xs]
     foldingFunction xs numberString = read numberString:xs
